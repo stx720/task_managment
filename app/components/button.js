baa453b60eaf4caa49481a5bf06b3ec84e785bed
stx720 <michal.obrebski2002@gmail.com>
2024-02-04 22:29:10 +0100

    added transition

--- app/components/button.js
+++ app/components/button.js
@@ -1,9 +1,9 @@
 const Button = () => {
-    return (
-        <button className="flex flex-row bg-gray-400 mt-14 hover:bg-gray-600 text-white text-xl lg:text-3xl font-bold py-4 px-6 rounded-full min-w-max">
-        Get Started<h1 className="ml-2 animate-shake text-gray-200">➜</h1>
-      </button>
-    );
-  };
-  
-  export default Button;
+  return (
+    <button className="flex flex-row bg-gray-400 mt-14 transition duration-300 hover:bg-gray-600 text-white text-xl lg:text-3xl font-bold py-4 px-6 rounded-full min-w-max">
+      Get started<h1 className="ml-2 animate-shake text-gray-200">➜</h1>
+    </button>
+  );
+};
+
+export default Button;
