1cafd24744c42ae8d54e189dbab9535d8c403e21
stx720 <michal.obrebski2002@gmail.com>
2024-02-08 02:04:51 +0100

    home link
    Logo now takes home after clicking it

--- app/components/home-logo.js
+++ app/components/home-logo.js
@@ -1,6 +1,9 @@
+import Link from "next/link";
+
 const Logo = () => {
     return (
-      <div className="font-custom flex flex-row items-center cursor-default">
+      <Link href="/" className="cursor-pointer">
+      <div className="font-custom flex flex-row items-center">
         <div className="flex flex-row justify-center">
           <h1 className="flex text-center transition text-4xl md:text-7xl lg:text-8xl  hover:scale-125">T</h1>
           <h1 className="flex text-center transition text-4xl md:text-7xl lg:text-8xl hover:scale-125">A</h1>
@@ -8,13 +11,14 @@ const Logo = () => {
           <h1 className="flex text-center transition text-4xl md:text-7xl lg:text-8xl  hover:scale-125">K</h1>
           <h1 className="flex text-center transition text-4xl md:text-7xl lg:text-8xl  ">_</h1>
         </div>
-        <div className="flex flex-row justify-center cursor-default">
+        <div className="flex flex-row justify-center">
           <h1 className="flex text-center transition text-4xl md:text-7xl lg:text-8xl hover:scale-125">L</h1>
           <h1 className="flex text-center transition text-4xl md:text-7xl lg:text-8xl hover:scale-125">I</h1>
           <h1 className="flex text-center transition text-4xl md:text-7xl lg:text-8xl hover:scale-125">S</h1>
           <h1 className="flex text-center transition text-4xl md:text-7xl lg:text-8xl hover:scale-125">T</h1>
         </div>
       </div>
+      </Link>
     );
   };
   
